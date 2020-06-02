665b6d89100bcf454b7751acfdbdc968a857c54b
Darki <jw.winkelm.2005@gmail.com>
2020-06-02 16:06:37 +0200

    how

--- src/main/kotlin/com/kul/Main.kt
+++ src/main/kotlin/com/kul/Main.kt
@@ -1,20 +1,22 @@
 package com.kul
 
 import com.kul.config.ConfigReader
+import com.kul.config.KulConfig
 import com.kul.transformer.TransformerManager
 import java.io.File
+import javax.script.ScriptEngineManager
 
 fun main(args: Array<String>) {
 
-    if(args.isEmpty()) {
+    if (args.isEmpty()) {
         println("Please provide a jar file!")
         return
     }
 
     TransformerManager.init()
 
-    for(i in 0..args.size) {
-        if(args[i] == "-config") {
+    for (i in 0..args.size) {
+        if (args[i] == "-config") {
             ConfigReader.loadConfig(File(args[i + 1]))
             break
         }
