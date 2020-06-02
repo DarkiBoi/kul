8c72d95208ab88dcd9e063f1c4c24c26b49c591b
Darki <jw.winkelm.2005@gmail.com>
2020-06-02 14:43:38 +0200

    remove cringe

--- src/main/kotlin/com/kul/utils/RandomStringGenerator.kt
+++ src/main/kotlin/com/kul/utils/RandomStringGenerator.kt
@@ -3,7 +3,7 @@ package com.kul.utils
 object RandomStringGenerator {
 
     fun genRandomString(n: Int): String? {
-        val charSet = "鮉縑᱘晜骫炀嶾额諕臝箙䵆뾛腻㛞崙购袋㿦塚蓻닄箙䵆籟ꍉ鑹'燡ɨ戈ᬶ蕅헿N둿癠"
+        val charSet = "鮉縑᱘晜骫炀嶾额諕臝箙䵆뾛腻㛞崙购袋㿦塚蓻닄箙䵆籟ꍉ鑹燡ɨ戈ᬶ蕅헿N둿癠"
         val sb = StringBuilder(n)
         for (i in 0 until n) {
 
